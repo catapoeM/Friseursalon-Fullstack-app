4a06acc3e24dc23d01cbbaf4da5174f55321949c
Catalin_Poenaru <catalin_poenaru95@yahoo.com>
2026-01-09 18:13:21 +0100

    Admin route
    Updates on admin route / stylist schema

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -1,10 +1,45 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
-const adminSchema = new mongoose.Schema(
+const AdminSchema = new mongoose.Schema(
     {
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
     }
 );
 
-export default mongoose.model("Admin", adminSchema);
+const ServiceSchema = new mongoose.Schema({
+    serviceName: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    clientType: {
+        type: String,
+        enum: ["Woman", "Man", "Child"],
+        required: true
+    }
+})
+
+const StylistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    services: [ServiceSchema],
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+})
+
+const Admin = mongoose.model("Admin", AdminSchema);
+const Stylist = mongoose.model("StylistSchema", StylistSchema)
+
+export {Admin, Stylist}
